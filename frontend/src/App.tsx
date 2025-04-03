baa7e9fae9effad3974cc12306c846ca9b3ccacf
Micah Johnson <mjmikes@byu.edu>
2025-04-03 14:00:06 -0600

    add privacy page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import JoinPage from './pages/JoinPage'
-import AdminLayout from './components/admin/AdminLayout'
-import AdminHomePage from './pages/admin/AdminHomePage'
-import ManageSitePage from './pages/admin/ManageSitePage'
-import ManageUsersPage from './pages/admin/ManageUsersPage'
-import CookieConsent from './components/all_pages/CookieConsent'
-import ProtectedRoute from './components/all_pages/ProtectedRoute'
-import { AuthProvider } from './components/context/AuthContext'
-import AccountPage from './pages/AccountPage'
-import ShopPage from './pages/ShopPage'
-import AdminAccountPage from './pages/admin/AdminAccountPage'
-import AdminProtectedRoute from './components/all_pages/AdminProtectedRoute'
-import DetailsPage from './pages/DetailsPage'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import JoinPage from "./pages/JoinPage";
+import AdminLayout from "./components/admin/AdminLayout";
+import AdminHomePage from "./pages/admin/AdminHomePage";
+import ManageSitePage from "./pages/admin/ManageSitePage";
+import ManageUsersPage from "./pages/admin/ManageUsersPage";
+import CookieConsent from "./components/all_pages/CookieConsent";
+import ProtectedRoute from "./components/all_pages/ProtectedRoute";
+import { AuthProvider } from "./components/context/AuthContext";
+import AccountPage from "./pages/AccountPage";
+import ShopPage from "./pages/ShopPage";
+import AdminAccountPage from "./pages/admin/AdminAccountPage";
+import PrivacyPage from "./pages/PrivacyPage";
 
 function App() {
   return (
@@ -23,14 +21,28 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
-          <Route path="/details" element={<DetailsPage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
           <Route path="/Login" element={<LoginPage />} />
           <Route path="/Join" element={<JoinPage />} />
-          <Route path="/account" element={<ProtectedRoute><AccountPage /></ProtectedRoute>} />
+          <Route
+            path="/account"
+            element={
+              <ProtectedRoute>
+                <AccountPage />
+              </ProtectedRoute>
+            }
+          />
 
           {/* Protected Routes */}
-          <Route path="/admin" element={<AdminProtectedRoute><AdminLayout /></AdminProtectedRoute>}>
-            <Route index element={<ManageUsersPage />} />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <AdminLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<AdminHomePage />} />
             <Route path="site" element={<ManageSitePage />} />
             <Route path="users" element={<ManageUsersPage />} />
             <Route path="account" element={<AdminAccountPage />} />
@@ -42,4 +54,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
