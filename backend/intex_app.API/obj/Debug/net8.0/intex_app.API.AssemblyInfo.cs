a967005d6a3f79c82fa241dd48973f0e6b308dfb
JakeThomas7 <131414634+JakeThomas7@users.noreply.github.com>
2025-04-08 08:03:13 -0600

    Updating routes to backend

--- backend/intex_app.API/obj/Debug/net8.0/intex_app.API.AssemblyInfo.cs
+++ backend/intex_app.API/obj/Debug/net8.0/intex_app.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("intex_app.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f217a8222c95a13a716e62c821fc3b943b01fa2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aea1433db9201b6e3577b91a687708185c9e8cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("intex_app.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("intex_app.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/api/UsersAPI.tsx
+++ frontend/src/api/UsersAPI.tsx
@@ -1,7 +1,7 @@
 import User from "../types/User";
 
-const API_URL = 'https://localhost:5000';
-//const API_URL = 'https://api2.byjacobthomas.com'
+//const API_URL = 'https://localhost:5000';
+const API_URL = 'https://api2.byjacobthomas.com'
 
 export const updateProfile = async (user: User | null): Promise<void> => {
     const response = await fetch(`${API_URL}/Users/updateUserProfile`, {
