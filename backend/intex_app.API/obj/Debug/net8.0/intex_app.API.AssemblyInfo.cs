7aa2b09b27a358b57c4cad6a34f88b7d9270d8cb
ricewuh <131925311+ricewuh@users.noreply.github.com>
2025-04-09 09:20:55 -0600

    revert

--- backend/intex_app.API/obj/Debug/net8.0/intex_app.API.AssemblyInfo.cs
+++ backend/intex_app.API/obj/Debug/net8.0/intex_app.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("intex_app.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a967005d6a3f79c82fa241dd48973f0e6b308dfb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970df66030475d7db0d6323600ac955285dcd8ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("intex_app.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("intex_app.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
